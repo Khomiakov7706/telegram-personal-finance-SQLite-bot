abdee7266a4662fb4219e2174eaee64d082f093e
Ivan Khomiakov <iskhomiakov@gmail.com>
2023-11-08 12:42:28 +0700

    Removed proxi

--- server.py
+++ server.py
@@ -10,18 +10,17 @@ import expenses
 from categories import Categories
 from middlewares import AccessMiddleware
 
-
 logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
+""" PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
 PROXY_AUTH = aiohttp.BasicAuth(
     login=os.getenv("TELEGRAM_PROXY_LOGIN"),
     password=os.getenv("TELEGRAM_PROXY_PASSWORD")
-)
+) """
 ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
 
-bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
+bot = Bot(token=API_TOKEN) #, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
 dp = Dispatcher(bot)
 dp.middleware.setup(AccessMiddleware(ACCESS_ID))
 
